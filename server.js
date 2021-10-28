d0d2edc3f0c38d2bf6a58a56122644645405470c
ashish_mathew <ashishmathewamy009@gmail.com>
2021-10-28 16:40:12 +0530

    remove port from env

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
